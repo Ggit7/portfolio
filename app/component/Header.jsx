625b3a5fdfd73dd76b82fcc625b46b9bf2306a5e
Ggit7 <gourab.biit@gmail.com>
2025-01-15 09:18:50 +0530

    done

--- app/component/Header.jsx
+++ app/component/Header.jsx
@@ -19,10 +19,10 @@ const Header = () => {
       </p>
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href='#contact' 
-        className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'
+        className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 hover:-translate-y-1 duration-500'
         >Contact me<Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
 
-        <a href='/resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>
+        <a href='/resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 hover:-translate-y-1 duration-500'>
             my resume<Image src={assets.download_icon} alt='' className='w-4'/>
         </a> 
       </div>
